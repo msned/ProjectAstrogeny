5ce67982209119579132253859eb6649db4528cf
msnedec <msnedec@ncsu.edu>
2018-05-26 14:19:20 -0400

    Reshaped directories for empire entities and generation

--- source/MathConsts.d
+++
@@ -1,7 +0,0 @@
-module MathConsts;
-
-/** Radius of sun*/
-immutable double solarRad = 695700;
-immutable double astroUnit = 149597870;
-immutable double bolometricConstant = .2632;
-immutable double solTemp = 5770;
